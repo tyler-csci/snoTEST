26ec535bd25ada53bd5584683f7a401a95911f59
Tyler <tyler.csci@gmail.com>
2020-03-01 20:11:25 -0700

    updated home page

--- pages/index.vue
+++ pages/index.vue
@@ -5,78 +5,78 @@
                  style="background-image: url('img/header.jpg')">
             </div>
             <div class="content-center">
-                <h1 class="title">snoDEV</h1>
-                <div class="text-center">
-                    <a href="#pablo" class="btn btn-success btn-icon  btn-round">
-                        <i class="fab fa-facebook-square"></i>
+                <h2 class="title">snoDEV</h2>
+                <h3>BRINGING IDEAS TO LIFE</h3>
+                    <a href="https://www.linkedin.com/company/snodev" target="_blank" rel="noopener" class="btn btn-success btn-icon  btn-round">
+                        <i class="fab fa-linkedin"></i>
                     </a>
-                    <a href="#pablo" class="btn btn-success btn-icon btn-round">
+                    <a href="https://twitter.com/gosnodev" target="_blank" rel="noopener" class="btn btn-success btn-icon  btn-round">
                         <i class="fab fa-twitter"></i>
                     </a>
-                    <a href="#pablo" class="btn btn-success btn-icon btn-round">
-                        <i class="fab fa-google-plus"></i>
+                    <a href="https://www.facebook.com/gosnodev" target="_blank" rel="noopener" class="btn btn-success btn-icon btn-round">
+                        <i class="fab fa-facebook-f"></i>
                     </a>
-                </div>
+                    <a href="https://www.instagram.com/gosnodev" target="_bank" rel="noopener" class="btn btn-success btn-icon btn-round">
+                        <i class="fab fa-instagram"></i>
+                    </a>
             </div>
         </div>
-        <div class="section section-about-us">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-8 ml-auto mr-auto text-center">
-                        <h2 class="title">Who we are?</h2>
-                        <h5 class="description">According to the National Oceanic and Atmospheric Administration, Ted,
-                            Scambos, NSIDClead scentist, puts the potentially record low maximum sea ice extent tihs
-                            year down to low ice extent in the Pacific and a late drop in ice extent in the Barents
-                            Sea.</h5>
-                    </div>
-                </div>
-                <div class="separator separator-success"></div>
-                <div class="section-story-overview">
+        <div class="section">
+            <div class="about-description text-center">
+                <div class="container">
                     <div class="row">
-                        <div class="col-md-6">
-                            <!-- First image on the left side -->
-                            <div class="image-container image-left" style="background-image: url('img/bg38.jpg')">
-                                <p class="blockquote blockquote-primary">"Over the span of the satellite record, Arctic
-                                    sea ice has been declining significantly, while sea ice in the Antarctichas
-                                    increased very slightly"
-                                    <br>
-                                    <br>
-                                    <small>-NOAA</small>
-                                </p>
-                            </div>
-                            <!-- Second image on the left side of the article -->
-                            <div class="image-container image-left-bottom"
-                                 style="background-image: url('img/bg24.jpg')"></div>
+                        <div class="col-md-8 mr-auto ml-auto">
+                            <h2 class="title">Simpler. Faster. Better.</h2>
+                            <h4>BUILT FROM SCRATCH.</h4>
+                            <h4 class="description">SnoDEV uses latest technologies and industry best practices to ensure your website looks and 
+                                performs better than any other. Whether you need a personal site or something for business, SnoDEV is here to 
+                                provide you with a responsive, efficient, secure, and aesthetic website.
+                            </h4>
                         </div>
-                        <div class="col-md-5">
-                            <!-- First image on the right side, above the article -->
-                            <div class="image-container image-right"
-                                 style="background-image: url('img/bg39.jpg')"></div>
-                            <h3>So what does the new record for the lowest level of winter ice actually mean</h3>
-                            <p>The Arctic Ocean freezes every winter and much of the sea-ice then thaws every summer,
-                                and that process will continue whatever happens with climate change. Even if the Arctic
-                                continues to be one of the fastest-warming regions of the world, it will always be
-                                plunged into bitterly cold polar dark every winter. And year-by-year, for all kinds of
-                                natural reasons, there’s huge variety of the state of the ice.
-                            </p>
-                            <p>
-                                For a start, it does not automatically follow that a record amount of ice will melt this
-                                summer. More important for determining the size of the annual thaw is the state of the
-                                weather as the midnight sun approaches and temperatures rise. But over the more than 30
-                                years of satellite records, scientists have observed a clear pattern of decline,
-                                decade-by-decade.
-                            </p>
-                            <p>The Arctic Ocean freezes every winter and much of the sea-ice then thaws every summer,
-                                and that process will continue whatever happens with climate change. Even if the Arctic
-                                continues to be one of the fastest-warming regions of the world, it will always be
-                                plunged into bitterly cold polar dark every winter. And year-by-year, for all kinds of
-                                natural reasons, there’s huge variety of the state of the ice.
-                            </p>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="info info-hover">
+                                <div class="icon icon-info icon-circle">
+                                    <i class="now-ui-icons objects_globe"></i>
+                                </div>
+                                <h4 class="info-title">Single Page Applications</h4>
+                                <p class="description">Load once. After that, your users won't have to wait when they browse your site.</p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="info info-hover">
+                                <div class="icon icon-info icon-circle">
+                                    <i class="now-ui-icons education_atom"></i>
+                                </div>
+                                <h4 class="info-title">Server-Sided Rendering</h4>
+                                <p class="description">Better SEO, and faster time-to-content. SSR user experience is dramatically improved from standard websites.</p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="info info-hover">
+                                <div class="icon icon-info icon-circle">
+                                    <i class="now-ui-icons education_atom"></i>
+                                </div>
+                                <h4 class="info-title">Progressive Web Applications</h4>
+                                <p class="description">Install, load offline, and get the same experience as a mobile app.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        <div class="separator-line separator-primary"></div>
+        <div class="section">
+            <div class="about-description text-center">
+                <div class="container">
+                    <h5>People get a Ferrari not just for the looks, but also because of what lies under the hood.</h5>
+                </div>
+            </div>
+        </div>
+        
+        <div class="separator-line separator-primary"></div>
+
 
         <div class="testimonials-1 section-image" style="background-image: url('img/bg19.jpg')">
             <div class="container">
