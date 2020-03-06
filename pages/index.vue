538b4822af9b617f696795b94eb5af9c55ea5f49
Tyler <tyler.csci@gmail.com>
2020-03-05 20:15:52 -0700

    updated home page

--- pages/index.vue
+++ pages/index.vue
@@ -140,8 +140,10 @@
                 <div class="row">
                     <div class="col-lg-6 text-center ml-auto mr-auto col-md-8">
                         <form 
-                            data-netlify="true"
-                            data-netlify-honeypot="bot-field"
+                            class="index-form"
+                            name="index-form"
+                            action="/thankyou"
+                            method="post"
                         >
                         <input type="hidden" name="contact-us" value="contact-us" />
                             <fg-input
