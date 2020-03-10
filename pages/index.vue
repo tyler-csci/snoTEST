1316e83c682e1678880921aeb02d85e64187a183
Tyler <tyler.csci@gmail.com>
2020-03-10 17:31:28 -0600

    updated files

--- pages/index.vue
+++ pages/index.vue
@@ -132,49 +132,6 @@
                 </div>
             </div>
         </div>
-                
-        <div class="section section-contact-us text-center">
-            <div class="container">
-                <h2 class="title">Want to work with us?</h2>
-                <p class="description">Your project is very important to us.</p>
-                <div class="row">
-                    <div class="col-lg-6 text-center ml-auto mr-auto col-md-8">
-                        <form 
-                            name="contact-form"
-                            action="/thankyou"
-                            method="post"
-                            netlify
-                            netlify-honeypot="bot-field">
-                            <input type="hidden" name="contact-us" value="contact-us" />
-                              
-                            <fg-input
-                                name="name"
-                                class="input-lg"
-                                placeholder="First Name..."
-                                v-model="form.firstName"
-                                addon-left-icon="now-ui-icons users_circle-08">
-                            </fg-input>
-                            <fg-input
-                                name="email"
-                                class="input-lg"
-                                placeholder="Email Here..."
-                                v-model="form.email"
-                                addon-left-icon="now-ui-icons ui-1_email-85">
-                            </fg-input>
-                            <div class="textarea-container">
-                                <textarea class="form-control"
-                                        name="name" rows="4" cols="80"
-                                        v-model="form.message"
-                                        placeholder="Type a message..."></textarea>
-                            </div>
-                            <div class="send-button">
-                                <n-button type="success" round block size="lg">Send Message</n-button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 </template>
 <script>
